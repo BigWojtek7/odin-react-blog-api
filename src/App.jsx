8173fdda79b91c0e66ffa65a32df4fa50f4e38b5
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-10-02 11:09:49 +0200

    Add notification provider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import MainLayout from './layouts/MainLayout';
 import AuthProvider from './contexts/AuthProvider';
 import LoaderProvider from './contexts/LoaderProvider';
 import ModalProvider from './contexts/ModalProvider';
+import NotificationProvider from './contexts/NotificationProvider';
 
 function App() {
   return (
     <>
-      <ModalProvider>
-        <LoaderProvider>
-          <AuthProvider>
-            <MainLayout />
-          </AuthProvider>
-        </LoaderProvider>
-      </ModalProvider>
+      <NotificationProvider>
+        <ModalProvider>
+          <LoaderProvider>
+            <AuthProvider>
+              <MainLayout />
+            </AuthProvider>
+          </LoaderProvider>
+        </ModalProvider>
+      </NotificationProvider>
     </>
   );
 }
