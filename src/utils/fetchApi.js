6361cc51388d3d9200f544b207278c9e3e2c6b59
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-10-05 08:54:39 +0200

    Add check for error before response in fetchApi.js

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,15 +1,25 @@
 const requestWithNativeFetch = async (url, options) => {
-  const response = await fetch(url, options);
-  const responseData = await response.json();
+  try {
+    const response = await fetch(url, options);
 
-  if (!response.ok) {
+    if (!response.ok) {
+      const errorData = await response.json();
+      return {
+        success: false,
+        status: response.status,
+        msg: errorData?.msg || 'Something went wrong',
+      };
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (err) {
     return {
       success: false,
-      status: response.status,
-      msg: responseData.msg || 'Something went wrong',
+      status: 500,
+      msg: err.message || 'network error',
     };
   }
-  return responseData;
 };
 
 export default requestWithNativeFetch;
