cc362eda3fba66180d8b023a7cc71ba176866be8
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-10-30 20:05:51 +0100

    Add tests for Comment component

---
+++ src/components/Comment/Comment.test.jsx
@@ -0,0 +1,83 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Comment from './Comment';
+import { MemoryRouter } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import useModal from '../../hooks/useModal';
+import checkPermissions from '../../utils/checkPermissions';
+import useNotification from '../../hooks/useNotification';
+
+vi.mock('../../hooks/useAuth');
+vi.mock('../../hooks/useModal');
+vi.mock('../../utils/checkPermissions');
+vi.mock('../../hooks/useNotification');
+
+const mockSetComments = vi.fn();
+
+describe('Comment component tests', () => {
+  const commentProps = {
+    commentId: 1,
+    author: 'Author',
+    content: 'This is a comment',
+    date: '2024-10-29',
+    setComments: mockSetComments,
+  };
+
+  beforeEach(() => {
+    useAuth.mockReturnValue({
+      user: { name: 'wojtek', is_admin: false },
+      token: 'token',
+    });
+    useModal.mockReturnValue({ openModal: vi.fn(), closeModal: vi.fn() });
+    checkPermissions.mockReturnValue({ isAdmin: false });
+    useNotification.mockReturnValue({ addNotification: vi.fn() });
+  });
+
+  it('renders Comment component', () => {
+    render(
+      <MemoryRouter>
+        <Comment {...commentProps} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(commentProps.author)).toBeInTheDocument();
+    expect(screen.getByText(commentProps.content)).toBeInTheDocument();
+    expect(screen.getByText(commentProps.date)).toBeInTheDocument();
+    expect(screen.queryByText('Delete')).not.toBeInTheDocument();
+  });
+
+  it('renders Delete button for admin', () => {
+    useAuth.mockReturnValue({
+      user: { name: 'admin', is_admin: true },
+      token: 'token',
+    });
+    checkPermissions.mockReturnValue({ isAdmin: true });
+
+    render(
+      <MemoryRouter>
+        <Comment {...commentProps} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Delete')).toBeInTheDocument();
+  });
+
+  it('calls handleDeleteComment when Delete button is clicked', async () => {
+    // Mock the necessary functions
+    const mockOpenModal = vi.fn();
+
+    const user = userEvent.setup();
+    useModal.mockReturnValue({ openModal: mockOpenModal, closeModal: vi.fn() });
+    checkPermissions.mockReturnValue({ isAdmin: true });
+
+    render(
+      <MemoryRouter>
+        <Comment {...commentProps} />
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', { name: 'Delete' });
+    await user.click(button);
+    expect(mockOpenModal).toHaveBeenCalled();
+  });
+});
--- src/components/Post/Post.test.jsx
+++ src/components/Post/Post.test.jsx
@@ -4,24 +4,27 @@ import Post from './Post';
 import { MemoryRouter } from 'react-router-dom';
 
 import useAuth from '../../hooks/useAuth';
+import checkPermissions from '../../utils/checkPermissions';
 
 vi.mock('../../hooks/useAuth');
-
-const post = {
-  id: 1,
-  username: 'user',
-  title: 'Main title',
-  content: 'Some example content',
-  date_format: '13-10-2024 19:00',
-};
-
-beforeEach(() => {
-  useAuth.mockReset();
-});
+vi.mock('../../utils/checkPermissions.js');
 
 describe('Post component test', () => {
+  const post = {
+    id: 1,
+    username: 'user',
+    title: 'Main title',
+    content: 'Some example content',
+    date_format: '13-10-2024 19:00',
+  };
+
+  beforeEach(() => {
+    useAuth.mockReset();
+  });
+
   it('renders Post component in preview mode & user is no admin', () => {
     useAuth.mockReturnValue({ user: { name: 'wojtek', is_admin: false } });
+    checkPermissions.mockReturnValue({ isAdmin: false });
     render(
       <MemoryRouter>
         <Post post={post} isPreview={true} />
@@ -35,6 +38,7 @@ describe('Post component test', () => {
 
   it('renders Post component in preview mode & user is admin', () => {
     useAuth.mockReturnValue({ user: { name: 'wojtek', is_admin: true } });
+    checkPermissions.mockReturnValue({ isAdmin: true });
     render(
       <MemoryRouter>
         <Post post={post} isPreview={true} />
@@ -46,6 +50,7 @@ describe('Post component test', () => {
 
   it('renders Post component without preview mode', () => {
     useAuth.mockReturnValue({ user: { name: 'wojtek', is_admin: false } });
+    checkPermissions.mockReturnValue({ isAdmin: false });
     render(
       <MemoryRouter>
         <Post post={post} isPreview={false} />
