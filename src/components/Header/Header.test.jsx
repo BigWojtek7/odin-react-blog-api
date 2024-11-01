5ce41862c224ea58af86e0bb87c6b83b83880814
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-01 20:22:04 +0100

    Add userevent to Header components tests

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -3,35 +3,46 @@ import Header from './Header';
 import { beforeEach, describe } from 'vitest';
 import { MemoryRouter } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import checkPermissions from '../../utils/checkPermissions';
+import userEvent from '@testing-library/user-event';
 
 vi.mock('../../hooks/useAuth.js');
+vi.mock('../../utils/checkPermissions.js');
+
+const logOut = vi.fn();
 
 describe('testing Header component', () => {
   beforeEach(() => {
     useAuth.mockReturnValue({
       user: { username: 'wojtek', is_admin: false },
       token: 'token',
-      logOut: vi.fn(),
+      logOut: logOut,
     });
+    checkPermissions.mockReturnValue({ isAdmin: false });
   });
 
-  it('renders Header component with token', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('renders Header component', () => {
     render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>
     );
 
+    expect(screen.queryByText('New Post')).not.toBeInTheDocument();
     expect(screen.getByText('wojtek')).toBeInTheDocument();
-    // expect(screen.getByRole('Log out')).toBeInTheDocument();
     expect(screen.getByText('Daily Blog')).toBeInTheDocument();
+    expect(screen.getByLabelText('Log out')).toBeInTheDocument();
   });
 
   it('renders Header component without token', () => {
     useAuth.mockReturnValue({
       user: { username: 'wojtek', is_admin: false },
       token: null,
-      logOut: vi.fn(),
+      logOut: logOut,
     });
 
     render(
@@ -40,17 +51,20 @@ describe('testing Header component', () => {
       </MemoryRouter>
     );
 
+    expect(screen.queryByText('wojtek')).not.toBeInTheDocument();
+
     expect(screen.getByText('Log-in')).toBeInTheDocument();
     expect(screen.getByText('Sign Up')).toBeInTheDocument();
     expect(screen.getByText('Daily Blog')).toBeInTheDocument();
   });
 
-  it('renders Header component without admin permission', () => {
+  it('renders Header component with admin permission', () => {
     useAuth.mockReturnValue({
       user: { username: 'wojtek', is_admin: true },
-      token: null,
+      token: 'token',
       logOut: vi.fn(),
     });
+    checkPermissions.mockReturnValue({ isAdmin: true });
 
     render(
       <MemoryRouter>
@@ -59,7 +73,26 @@ describe('testing Header component', () => {
     );
 
     expect(screen.getByText('New Post')).toBeInTheDocument();
-    expect(screen.getByText('Sign Up')).toBeInTheDocument();
+    expect(screen.getByText('wojtek')).toBeInTheDocument();
     expect(screen.getByText('Daily Blog')).toBeInTheDocument();
+  });
+
+  it('calls logout on log out button click', async () => {
+    useAuth.mockReturnValue({
+      user: { username: 'wojtek', is_admin: true },
+      token: 'token',
+      logOut: logOut,
+    });
+
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    const user = userEvent.setup();
+    await user.click(screen.getByLabelText('Log out'));
+
+    expect(logOut).toHaveBeenCalledTimes(1);
   });
 });
