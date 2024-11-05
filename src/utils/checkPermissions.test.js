45b7ec431a0ec15126808e9f720fcd2687d4e6b8
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-05 08:12:07 +0100

    Add tests for PrivateRouter, checkPermission & requesWithNativeFetch

---
+++ src/utils/checkPermissions.test.js
@@ -0,0 +1,30 @@
+import checkPermissions from './checkPermissions';
+
+describe('checkPermissions', () => {
+  it('returns false for both isAdmin and canCreatePost when user is undefined', () => {
+    const permissions = checkPermissions();
+    expect(permissions.isAdmin).toBe(false);
+    expect(permissions.canCreatePost).toBe(false);
+  });
+
+  it('returns false for both isAdmin and canCreatePost when user has no roles', () => {
+    const user = {};
+    const permissions = checkPermissions(user);
+    expect(permissions.isAdmin).toBe(false);
+    expect(permissions.canCreatePost).toBe(false);
+  });
+
+  it('returns true for both isAdmin and canCreatePost when user is an admin', () => {
+    const user = { is_admin: true };
+    const permissions = checkPermissions(user);
+    expect(permissions.isAdmin).toBe(true);
+    expect(permissions.canCreatePost).toBe(true);
+  });
+
+  it('returns false for isAdmin and true for canCreatePost when user is an editor', () => {
+    const user = { is_editor: true };
+    const permissions = checkPermissions(user);
+    expect(permissions.isAdmin).toBe(false);
+    expect(permissions.canCreatePost).toBe(true);
+  });
+});
