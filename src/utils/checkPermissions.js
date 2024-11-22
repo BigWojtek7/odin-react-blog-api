bb92fa16c7b14c846b8c8b11ac1f0b9a20ec294a
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-22 21:43:34 +0100

    Add configs & install dependencies  for ts

--- src/utils/checkPermissions.js
+++
@@ -1,8 +0,0 @@
-const checkPermissions = (user = {}) => {
-  return {
-    isAdmin: !!user?.is_admin,
-    canCreatePost: !!user?.is_admin || !!user?.is_editor,
-  };
-};
-
-export default checkPermissions;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
   },
-})
+});
