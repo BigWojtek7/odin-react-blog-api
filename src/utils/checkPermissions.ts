fac8cb31dd3b71843d0ef66959fc79794cf88d4e
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-23 19:49:21 +0100

    Add types for vitest & jest

--- src/utils/checkPermissions.ts
+++ src/utils/checkPermissions.ts
@@ -1,4 +1,18 @@
-const checkPermissions = (user = {}) => {
+interface User {
+  is_admin: boolean;
+  is_editor: boolean;
+}
+interface Permissions {
+  isAdmin: boolean;
+  canCreatePost: boolean;
+}
+
+const defaultUser: User = {
+  is_admin: false,
+  is_editor: false,
+};
+
+const checkPermissions = (user: User = defaultUser): Permissions => {
   return {
     isAdmin: !!user?.is_admin,
     canCreatePost: !!user?.is_admin || !!user?.is_editor,
