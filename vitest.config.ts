861167f297fa8a59cb9cb3a0ebb34bf88bec5120
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-23 08:33:16 +0100

    Split configurations file into vite & vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup',
+  },
 });
