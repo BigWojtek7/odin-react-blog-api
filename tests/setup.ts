12c501226495f871c76b52c1dc16f45c456e6b51
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-24 21:40:34 +0100

    Add useAuth & checkpermissions as global mocks

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,24 @@
-import { expect, afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import * as matchers from "@testing-library/jest-dom/matchers";
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { Mock} from 'vitest';
 
 expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
 });
+
+import useAuth from '../src/contexts/Auth/useAuth';
+import checkPermissions from '../src/utils/checkPermissions';
+import { vi } from 'vitest';
+
+vi.mock('../src/contexts/Auth/useAuth', () => ({
+  __esModule: true,
+  default: vi.fn(), // Mockujemy funkcję useAuth
+}));
+
+global.mockedUseAuth = useAuth as Mock;
+global.mockedCheckPermissions = checkPermissions as Mock;
+
+
